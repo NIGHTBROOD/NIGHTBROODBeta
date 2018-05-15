1837ea8d1bde52dec66d70e3895e1bfa3d45cf41
damanzero <35621048+damanzero@users.noreply.github.com>
2018-05-15 21:54:49 +0400

    Updating Relic Wait time
    Changed Relic Wait time from 1 week to 1 day for both stage 3 & 4

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -98,8 +98,8 @@ LandKingSystem_HQ = 0;
              DYNA_LEVEL_MIN = 65;       -- level min for entering in Dynamis
     TIMELESS_HOURGLASS_COST = 500000;   -- cost of the timeless hourglass for Dynamis.
      CURRENCY_EXCHANGE_RATE = 100;      -- X Tier 1 ancient currency -> 1 Tier 2, and so on.  Certain values may conflict with shop items.  Not designed to exceed 198.
-RELIC_2ND_UPGRADE_WAIT_TIME = 604800;      -- wait time for 2nd relic upgrade (stage 2 -> stage 3) in seconds. 604800s = 1 RL week.
-RELIC_3RD_UPGRADE_WAIT_TIME = 295200;      -- wait time for 3rd relic upgrade (stage 3 -> stage 4) in seconds. 295200s = 82 hours.
+RELIC_2ND_UPGRADE_WAIT_TIME = 86400;      -- wait time for 2nd relic upgrade (stage 2 -> stage 3) in seconds. 86400s = 1 day.
+RELIC_3RD_UPGRADE_WAIT_TIME = 86400;      -- wait time for 3rd relic upgrade (stage 3 -> stage 4) in seconds. 86400 = 1 day.
 FREE_COP_DYNAMIS = 1 ; -- Authorize player to entering inside COP Dynamis without completing COP mission ( 1 = enable 0= disable)
 
 -- QUEST/MISSION SPECIFIC SETTINGS
