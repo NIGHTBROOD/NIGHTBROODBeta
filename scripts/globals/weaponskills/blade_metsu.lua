e10e3ee4c4d27202435c56cc122181f32ffbadf2
damanzero <35621048+damanzero@users.noreply.github.com>
2018-06-20 22:24:15 +0400

    Update blade_metsu.lua

--- scripts/globals/weaponskills/blade_metsu.lua
+++ scripts/globals/weaponskills/blade_metsu.lua
@@ -21,8 +21,8 @@ require("scripts/globals/weaponskills");
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     local params = {};
     params.numHits = 1;
-    params.ftp100 = 3; params.ftp200 = 3; params.ftp300 = 3;
-    params.str_wsc = 0.0; params.dex_wsc = 0.6; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
+    params.ftp100 = 3.4; params.ftp200 = 3.4; params.ftp300 = 3.4;
+    params.str_wsc = 0.0; params.dex_wsc = 1.2; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
     params.crit100 = 0.0; params.crit200 = 0.0; params.crit300 = 0.0;
     params.canCrit = false;
     params.acc100 = 0.0; params.acc200= 0.0; params.acc300= 0.0;
